c0d7a0e2a58453abe003e921f4da57e0b95dfc63
Dauda Samson Ayomide <daudasamson2214@gmail.com>
2025-01-07 12:54:31 +0100

    Using the typeof operator

--- script.js
+++ script.js
@@ -24,3 +24,7 @@ console.log(myPhone)
 
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Samson');
